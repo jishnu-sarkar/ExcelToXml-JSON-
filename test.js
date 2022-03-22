cbdedd147677f49229bb6af793e8193a874681b8
jishnu-sarkar <jishnu.sarkar@bitcanny.com>
2022-03-23 00:11:52 +0530

    read & convert

--- test.js
+++ test.js
@@ -1,61 +1,44 @@
+var XLSX = require("xlsx");
 
-const js2xmlparser = require("js2xmlparser");
-const xml2js = require("xml2js");
+var workbook = XLSX.readFile("Sample.xlsx", {
+  cellDates: true,
+  dateNF: "dd-mm-yyyy;@",
+});
+var sheet_name_list = workbook.SheetNames;
+var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], {
+  header: 0,
+  defval: "",
+  sheetStubs: true,
+  raw: false,
+});
+console.log(xlData);
 
-const demoJson = require("./output.json");
-
-
-xlsxj = require("xlsx-to-json");
-
-const check = xlsxj(
-  {
-    input: "Sample.xlsx",
-    output: "output.json",
-  },
-  function (err, result) {
-    if (err) {
-      console.error(err);
-    }
-    // else {
-    //   console.log(result);
-    // }
-  }
-);
-
-// console.log(1);
-// console.log(check);
-
-const demoJson = require("./output.json");
-// console.log(typeof demoJson);
-// console.log(Object.values(demoJson));
-console.log(demoJson.length);
 let count = 0;
 
-let finalJson = { Clients: { Client: [] } };
+let finalData = { Clients: { Client: [] } };
 
-// let tempDataSet = {};
-
-demoJson.forEach((elements) => {
-
+xlData.forEach((elements) => {
+  let tempDataSet = {};
   tempDataSet.Account = {};
 
   Object.keys(elements).forEach((element) => {
     if (element.includes("Account:")) {
+      // if (elements[element] === "") console.log(1);
       tempDataSet.Account[
         element.replace("Account:", "").trim().replace(/ /g, "")
       ] = elements[element];
     } else {
+      // if (elements[element] === "") console.log(2);
       tempDataSet[element.replace(/ /g, "")] = elements[element];
     }
   });
 
-  console.log(tempDataSet);
-  finalJson.Clients.Client.push(tempDataSet);
+  // console.log(tempDataSet);
+  // console.log("/n");
+  finalData.Clients.Client.push(tempDataSet);
 });
-console.log(typeof finalJson);
-console.log(finalJson);
-
-
+console.log(finalData);
+console.dir(finalData, { depth: null });
 //-------------------------------------------------------
 
 let builder = require("xmlbuilder");
@@ -72,10 +55,9 @@ let builder = require("xmlbuilder");
 // };
 
 const xml = builder
-  .create(finalJson, { encoding: "utf-8" })
+  .create(finalData, { encoding: "utf-8" })
   .end({ pretty: true });
 
 console.log(xml);
 
 //---------------------------------------------------------
-
